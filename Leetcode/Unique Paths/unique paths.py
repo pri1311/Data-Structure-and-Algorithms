27f651ecc2b8a987d6f847e91bf2bb32d71e56dd
Priya Nagda <64613009+pri1311@users.noreply.github.com>
2021-03-26 20:09:20 +0530

    Update unique paths.py

--- Leetcode/Unique Paths/unique paths.py
+++ Leetcode/Unique Paths/unique paths.py
@@ -6,4 +6,4 @@ class Solution:
         if (m, n) not in self.uniquePathsMap.keys():
             # the unique paths are sum of the unique paths form the below or right cell. Cache the result.
             self.uniquePathsMap[(m, n)] = self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
-        return self.uniquePathsMap[(m, n)
+        return self.uniquePathsMap[(m, n)]
